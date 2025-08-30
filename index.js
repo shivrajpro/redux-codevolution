e71400b4304e5b3c2bec15e7c0f661bb5963c833
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2025-08-30 23:59:08 +0530

    add ice-cream in existing reducer

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const bindActionCreators = redux.bindActionCreators;
 
 const CAKE_ORDERED = "CAKE_ORDERED";
 const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const ICECREAM_ORDERED = "ICECREAM_ORDERED";
+const ICECREAM_RESTOCKED = 'ICECREAM_RESTOCKED';
 
 function orderCake() {
     return { type: CAKE_ORDERED, payload: 1 };
@@ -18,8 +20,23 @@ function restockCake(qty = 1) {
     }
 }
 
+function orderIceCream() {
+    return {
+        type: ICECREAM_ORDERED,
+        payload: 1
+    }
+}
+
+function restockIceCream(qty = 1) {
+    return {
+        type: ICECREAM_RESTOCKED,
+        payload: qty
+    }
+}
+
 const initialState = {
-    numOfCakes: 10
+    numOfCakes: 10,
+    numOfIceCreams: 20
 }
 
 const reducer = (state = initialState, action) => {
@@ -35,13 +52,25 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 numOfCakes: state.numOfCakes + action.payload
             }
+
+        case ICECREAM_ORDERED:
+            return {
+                ...state,
+                numOfIceCreams: state.numOfIceCreams - 1
+            }
+
+        case ICECREAM_RESTOCKED:
+            return {
+                ...state,
+                numOfIceCreams: state.numOfIceCreams + action.payload
+            }
         default:
             return state;
     }
 }
 
 const store = createStore(reducer);
-const dispatch = bindActionCreators({ orderCake, restockCake }, store.dispatch);
+const dispatch = bindActionCreators({ orderCake, restockCake, orderIceCream, restockIceCream }, store.dispatch);
 console.log("Initial State", store.getState());
 
 const unsubscribe = store.subscribe(() => console.log("Updated state", store.getState()));
@@ -56,5 +85,9 @@ dispatch.orderCake();
 dispatch.orderCake();
 dispatch.restockCake();
 dispatch.restockCake(2);
+
+dispatch.orderIceCream();
+dispatch.orderIceCream();
+dispatch.restockIceCream(2);
 
 unsubscribe();
